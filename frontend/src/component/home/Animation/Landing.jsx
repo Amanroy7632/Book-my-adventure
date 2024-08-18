f69afd4167b3a8627083c506011703d88be5d30d
Aman Roy <yadavaman7632@gmail.com>
2024-08-18 16:29:20 +0530

    fixes the vercel issue

--- frontend/src/component/home/Animation/Landing.jsx
+++ frontend/src/component/home/Animation/Landing.jsx
@@ -3,29 +3,29 @@ import "./pre.css"
 const Landing = () => {
   return (
     <div className=' bg-[#BD3B4A] w-screen h-screen overflow-hidden'>
-      <div class='body'>
+      <div className='body'>
         <span>
             <span></span>
             <span></span>
             <span></span>
             <span></span>
         </span>
-        <div class='base'>
+        <div className='base'>
             <span></span>
-            <div class='face'></div>
+            <div className='face'></div>
         </div>
     </div>
-    <div class='longfazers'>
+    <div className='longfazers'>
         <span></span>
         <span></span>
         <span></span>
         <span></span>
     </div>
-    {/* <h2 className=" text-3xl font-semibold text-white">Welcome To India's No. 1 Online Bus Ticket Booking Site</h2> */}
-    {/* <h1 className=" text-5xl font-semibold  text-orange-500">Book <span className=" text-white">my</span> <span
-            className=" underline underline-offset-2 text-green-600">Adventure</span></h1> */}
-    <h1>Loading...</h1>
-    {/* <div className=' w-4 h-4 rounded-full border-r-2 border-l-2 border-gray-200 animate-spin'></div> */}
+    {/* <h2 classNameName=" text-3xl font-semibold text-white">Welcome To India's No. 1 Online Bus Ticket Booking Site</h2> */}
+    {/* <h1 classNameName=" text-5xl font-semibold  text-orange-500">Book <span classNameName=" text-white">my</span> <span
+            classNameName=" underline underline-offset-2 text-green-600">Adventure</span></h1> */}
+    <h1 className='loading-h1' >Loading...</h1>
+    {/* <div classNameName=' w-4 h-4 rounded-full border-r-2 border-l-2 border-gray-200 animate-spin'></div> */}
     </div>
   )
 }
