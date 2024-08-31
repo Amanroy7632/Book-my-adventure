3304bfa2b8d2308f7129e270b708ec893390ba18
Aman Roy <yadavaman7632@gmail.com>
2024-08-31 12:56:51 +0530

    fixed logo issue

--- frontend/src/Admin/src/components/Sidebar.jsx
+++ frontend/src/Admin/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../../../component/logo/Logo";
+import Logo from "../../public/bus.png"
 import { FaAlignLeft } from "react-icons/fa";
 
 function Sidebar({ className = "", children,toggleSidebar }) {
@@ -8,8 +8,9 @@ function Sidebar({ className = "", children,toggleSidebar }) {
     <div
       className={`w-64 ${className} bg-gray-800 text-white h-screen overflow-y-scroll`}
     >
-      <div className="p-4 text-lg font-bold relative">
-        <Logo className=" bg-blend-darken rounded-full overflow-hidden" />
+      <div className="p-4 pb-0 text-lg font-bold relative">
+        {/* <Logo className=" bg-blend-darken rounded-full overflow-hidden" /> */}
+        <div  className=" w-20"><img src={Logo} alt="logo" /></div>
         <FaAlignLeft onClick={toggleSidebar} className=" hidden max-md:block absolute top-6 cursor-pointer right-1"/>
       </div>
       <nav className="mt-4">{children}</nav>
