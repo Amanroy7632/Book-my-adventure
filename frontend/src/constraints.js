37056cc30e8fc14da4e6906e27b01ee36265e24b
Aman Roy <yadavaman7632@gmail.com>
2024-10-09 23:36:30 +0530

    Loging by refreshing fixed finally

--- frontend/src/constraints.js
+++ frontend/src/constraints.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://book-my-adventure.onrender.com/api/v1"
-export const BASE_URL = "http://localhost:8000/api/v1"
+export const BASE_URL = "https://book-my-adventure.onrender.com/api/v1"
+// export const BASE_URL = "http://localhost:8000/api/v1"
